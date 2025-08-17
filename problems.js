058621daee3f2afec135b8b9e6dbd8c7a6055309
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-08-18 01:01:20 +0600

    solve problem 3 and 4

--- problems.js
+++ problems.js
@@ -21,4 +21,51 @@ function countVowels(prop){
     return count;
 }
 
-// problem-3
+
+
+
+
+// Problem 3: Check for Palindrome
+// Write a function that checks if a string is a palindrome (reads the same forward and backward).
+
+// Example:
+
+// Input: "madam"
+// Output: true
+// Input: "hello"
+// Output: false
+
+function isPalindrome(prop){
+    let reversed = '';
+    for (let i = prop.length -1; i >= 0; i--){
+        reversed = reversed + prop[i];
+    }
+    return prop === reversed;
+}
+
+
+
+
+
+// Problem 4: Find the Maximum Number
+// Write a function that takes an array of numbers and returns the largest number.
+
+// Example:
+
+// Input: [5, 1, 9, 3]
+// Output: 9
+
+function findMax(array){
+    if(array.length === 0){
+        return null;
+    }
+    let max = array[0];
+    for(let i = 1; i < array.length; i++){
+        if(array[i] > max){
+            max = array[i];
+        }
+    }
+    return max
+}
+
+
